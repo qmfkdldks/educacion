d6fc6f14dd3a0881cde0065fc41321b4c61fb505
Brian Kang <qmfkdldks@gmail.com>
2017-03-23 16:54:39 -0300

    Ckeditor configuration

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,5 +10,6 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 # Rails.application.config.assets.precompile += %w( ckeditor/filebrowser/thumbs/xls.gif )
-Rails.application.config.assets.precompile += Ckeditor.assets
-Rails.application.config.assets.precompile += %w(ckeditor/*)
+# Rails.application.config.assets.precompile += Ckeditor.assets
+# Rails.application.config.assets.precompile += %w(ckeditor/*)
+# Rails.application.config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
