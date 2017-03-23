02747f941d67db68128489632ff7d1469d33c845
Brian Kang <qmfkdldks@gmail.com>
2017-03-23 18:03:43 -0300

    Navbar page-scroll selector error debugged.

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module Educacion
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.assets.precompile += Ckeditor.assets
-	config.assets.precompile += %w(ckeditor/**/*) 
+	Rails.application.config.assets.precompile += %w(ckeditor/* ckeditor/lang/*)
 	config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
   end
 end
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,6 +10,3 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 # Rails.application.config.assets.precompile += %w( ckeditor/filebrowser/thumbs/xls.gif )
-# Rails.application.config.assets.precompile += Ckeditor.assets
-# Rails.application.config.assets.precompile += %w(ckeditor/*)
-# Rails.application.config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
