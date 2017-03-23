9d7e48e13dc3be09a5565b58bf6fd1912f70c1ce
Brian Kang <qmfkdldks@gmail.com>
2017-03-23 19:04:50 -0300

    Ckeditor precompile assets

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Educacion
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    # config.assets.precompile += Ckeditor.assets
+    config.assets.precompile += Ckeditor.assets
 	# config.assets.precompile += %w( ckeditor/* )
 	# config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
   end
